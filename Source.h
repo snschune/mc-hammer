fed378e0dade56934cc83b2dcf910634dd6335e6
Evan Leppink <eleppink@Umich.edu>
2017-11-30 11:09:10 -0500

    Added pointer

--- Source.h
+++ Source.h
@@ -1,12 +1,13 @@
 #include <tuple>
 #include <vector>
+#include <memory>
 
 #ifndef _SOURCE_HEADER_
 #define _SOURCE_HEADER_
 
 class Source {
 public:
-	std::vector<std::tuple <double, double, double, double, double, double> > pstack; 
+	std::shared_ptr<std::vector<std::tuple <double, double, double, double, double, double> > > pstack; 
   	//Tuple includes x,y,z,mu,phi,energy
 
 	std::vector<std::tuple <double, double, double, double, double, double> > source();
