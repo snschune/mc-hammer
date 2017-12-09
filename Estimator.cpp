47441793d9f20cf72e71b291773ac5dd18a55dbc
Kyle Beyer <beykyle@umich.edu>
2017-12-09 17:53:13 -0500

    multi group collision tally built, relatively untested

--- Estimator.cpp
+++ Estimator.cpp
@@ -18,9 +18,9 @@
 
 
 //estimator functions
-void estimator::update(Part_ptr pi , Part_ptr pf , log_ptr rxns) {};
+void Estimator::score(Part_ptr pi ) {};
 
-double estimator::dist(point p1 ,point p2) {
+double Estimator::dist(point p1 ,point p2) {
     return std::sqrt(
                 std::pow(p1.x - p2.x , 2) + 
                 std::pow(p1.y - p2.y , 2) + 
@@ -29,7 +29,7 @@ double estimator::dist(point p1 ,point p2) {
 };
 
 //constructor
-meshTally::meshTally(string comment, string type,  vector<int> numBins) : estimator(comment, type) {
+MeshTally::MeshTally(string comment, string type,  vector<int> numBins) : Estimator(comment, type) {
     vector<vector<vector<double>>> setMesh(numBins.at(0),
            vector<vector<double>>(numBins.at(1),
                    vector<double>(numBins.at(2)))
@@ -38,26 +38,85 @@ meshTally::meshTally(string comment, string type,  vector<int> numBins) : estima
 };
 
 //functions
-void meshTally::update(Part_ptr pi , Part_ptr pf , log_ptr rxns){
+void MeshTally::score(Part_ptr pi , Part_ptr pf ){
 
 };
 
 //constructor
-surfaceTally::surfaceTally(string comment , string type , Surf_ptr surfi ) : estimator(comment , type) , surf(surfi) {};
+SurfaceTally::SurfaceTally(string comment , string type , Surf_ptr surfi ) : Estimator(comment , type) , surf(surfi) {};
 
 //functions
-void surfaceTally::update(Part_ptr pi , Part_ptr pf , log_ptr rxns) {
+void SurfaceTally::score(Part_ptr pi , Part_ptr pf ) {
     bool cross = true;
     if (cross == true) {tally++;};
 };
 
 //constructor
-cellTally::cellTally(string comment , string type, Cell_ptr celli) : estimator(comment , type) , cell(celli) {
+CellTally::CellTally(string comment , string type, Cell_ptr celli) : Estimator(comment , type) , cell(celli) {
     tally = 0.0;
 };
 
 //functions
-void cellTally::update(Part_ptr pi , Part_ptr pf , log_ptr rxns) {
+void CellTally::score(Part_ptr pi , Part_ptr pf ) {
     tally +=  dist(pi->getPos() , pf->getPos());
 };
+
+
+//constructor
+CollisionTally::CollisionTally(string comment , string type, Cell_ptr celli) : Estimator(comment , type) , cell(celli) {
+    numGroups = 8; //TODO no
+
+    currentHistTally.resize(numGroups);
+    histTally.resize(numGroups);
+    histTallySqr.resize(numGroups);
+};
+
+//functions
+void CollisionTally::score(int groupi ) {
+     currentHistTally.at(groupi-1)  +=  1.0;
+};
+
+void CollisionTally::newHist() {
+    
+    vector <double> squares;
+    
+    // for each group, get the square of the tally in this history
+    for (int group = 1; group < numGroups;  ++group) {
+        squares.push_back(pow(currentHistTally.at(group-1),2));
+    }
+
+    // for each group, append the current history tally and square tally to their tracking vectors
+    for (int group = 1; group < numGroups;  ++group) {
+        histTally.at(group-1).push_back( currentHistTally.at(group-1) );
+        histTallySqr.at(group-1).push_back( squares.at(group-1)       );
+    }
+};
+
+        
+VectorEstimator CollisionTally::getFlux() {
+    
+    vector <double> flux(numGroups);
+    vector <double> unce(numGroups);
+   
+    
+    // for each group sum the tallies and square tallies over the histories
+    for (int group = 1; group < numGroups;  ++group) {
+        flux.at(group-1) = std::accumulate(histTally.at(group-1).begin(), histTally.at(group-1).end(), 0);
+        unce.at(group-1) = std::accumulate(histTallySqr.at(group-1).begin(), histTallySqr.at(group-1).end(), 0);
+    }
+
+    // for each group find the uncertainty
+    for (int group = 1; group < numGroups;  ++group) {
+        unce.at(group-1) = pow(flux.at(group-1),2) - unce.at(group-1);
+    }
+    
+    VectorEstimator fluxEstimator;
+
+    fluxEstimator.value = flux;
+    fluxEstimator.uncertainty = unce;
+
+    return(fluxEstimator);
+    
+};
+
 
--- Estimator.h
+++ Estimator.h
@@ -2,13 +2,20 @@
  * Author: Kyle Beyer
  * Last Updated: 11/30/17
  *
- * This class can:
+ * This class will eb able to:
  *  measure the path length density of particles in a 3D mesh
  *  tally the path length density of particles in a cell
  *  tally the number of particles crossing a surface
+ *
+ *  Interfaces with Transport class
+ *  A vector of each type of estimator will live in Geometry
+ *  Cell and collision tallies belong to Cells
+ *  Surface tallies belong to surfaces
+ *  Mesh tallies belong to Geometry
  */
 
 //TODO get functions, with gettally returning an uncertainty as well
+//TODO add tally as private member of estimator, and getTally() as virtual public member
 
 //TODO add energy dependence
 //TODO add material specificity
@@ -16,6 +23,12 @@
 
 #ifndef _ESTIMATOR_HEADER_
 #define _ESTIMATOR_HEADER_
+
+#include <iostream>
+#include <memory> 
+#include <stack>
+#include <limits>
+#include <numeric>
 
 #include "Point.h"
 #include "Particle.h"  
@@ -32,8 +45,17 @@ typedef std::shared_ptr<Material>  Mat_ptr;
 typedef std::shared_ptr<surface>   Surf_ptr;
 typedef std::shared_ptr<Cell>      Cell_ptr;
 typedef std::shared_ptr<rxnLog>     log_ptr;
+typedef std::shared_ptr<point>    Point_ptr;
 
-class estimator {
+struct VectorEstimator {
+    vector < double > value , uncertainty;
+};
+
+struct ScalarEstimator {
+    double value , uncertainty;
+};
+
+class Estimator {
   private:
     std::string estimator_name;
     std::string estimator_type;
@@ -44,47 +66,71 @@ class estimator {
     double dist(point p1 , point p2);
 
   public:
-     estimator( string comment , string type) : estimator_name(comment) , estimator_type(type) {};
-    ~estimator() {};
+     Estimator( string comment , string type) : estimator_name(comment) , estimator_type(type) {};
+    ~Estimator() {};
     
     virtual std::string name() { return estimator_name; };
     virtual std::string type() { return estimator_type; };
 
-    virtual void update(Part_ptr pi , Part_ptr pf , log_ptr rxns );
+    virtual void score(Part_ptr pi );
 };
 
 //TODO create mesh class, use 1D array and lookup functions
 
-class meshTally : public estimator {
+class MeshTally : public Estimator {
   private:
       vector < vector < vector < double > > > mesh;
   public:
-     meshTally(string comment, string type , vector<int> numBins); 
-    ~meshTally() {};
+     MeshTally(string comment, string type , vector<int> numBins); 
+    ~MeshTally() {};
 
-     void update(Part_ptr pi , Part_ptr pf , log_ptr rxns);
+     void score(Part_ptr pi , Part_ptr pf );
 };
 
-class surfaceTally : public estimator {
+class SurfaceTally : public Estimator {
     private:
         double   tally;
         Surf_ptr surf;
     public:
-         surfaceTally(string comment , string type, Surf_ptr surfi );
-        ~surfaceTally() {};
+         SurfaceTally(string comment , string type, Surf_ptr surfi );
+        ~SurfaceTally() {};
         
-        void update(Part_ptr pi , Part_ptr pf , log_ptr rxns );
+        void score(Part_ptr pi , Part_ptr pf );
 };
 
-class cellTally : public estimator {
+class CellTally : public Estimator {
     private:
         double     tally;
         Cell_ptr   cell;
     public:
-         cellTally(string comment , string type , Cell_ptr celli);
-        ~cellTally() {};
+         CellTally(string comment , string type , Cell_ptr celli);
+        ~CellTally() {};
 
-         void update(Part_ptr pi , Part_ptr pf , log_ptr rxns);
+         void score(Part_ptr pi , Part_ptr pf);
 };
+
+class CollisionTally : public Estimator {
+    private:
+        vector < double >              currentHistTally;
+        vector < vector < double > >   histTally;
+        vector < vector < double > >   histTallySqr;
+            
+        int numGroups;
+
+        Mat_ptr        mat;
+        Cell_ptr       cell;
+    public:
+        CollisionTally(string comment , string type, Cell_ptr celli);
+       ~CollisionTally() {};
+
+        void score( int groupi );
+        void newHist();
+
+        Mat_ptr  getMat()  { return(mat);  };
+        Cell_ptr getcell() { return(cell); };
+
+        VectorEstimator getFlux();
+};
+
 
 #endif
