fad47b493389ebb160e576123ae2efc3ce30adc0
esgonzalez <evangonzalez.mail@gmail.com>
2018-01-31 18:57:40 -0500

    Deleted QuadSolver.h and QuadSolver.cpp (added function to Utility.h and Utility.cpp)

--- QuadSolver.h
+++
@@ -1,8 +0,0 @@
-#ifndef _QUADSOLVER_HEADER_
-#define _QUADSOLVER_HEADER_
-
-// return largest positive root of quadratic equation with coefficients a, b, c
-// if both roots negative or complex, return a really big number
-double quad_solve( double a, double b, double c );
-
-#endif
