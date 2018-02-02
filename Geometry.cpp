62caea2d96486e80ee9006f258756ced10965cba
bdruci <bdruci@umich.edu>
2018-02-02 16:19:40 -0500

    fixed out of mesh issues (mesh is in meters)

--- Geometry.cpp
+++ Geometry.cpp
@@ -69,14 +69,14 @@ void Geometry::setup( std::string filename , int num_groups, bool loud )
         readXS( filename, num_groups, loud );
 
 		//surface bounbdaries
-		Surf_ptr sphere1 = make_shared<sphere>("sphere1", 0.0, 0.0, 0.0, 1.4935);
-		Surf_ptr sphere2 = make_shared<sphere>("sphere2", 0.0, 0.0, 0.0, 4.4935);
-		Surf_ptr xplane1 = make_shared<plane> ("xplane1", 1.0, 0.0, 0.0, -40.0);
-		Surf_ptr xplane2 = make_shared<plane> ("xplane2", 1.0, 0.0, 0.0, 40.0);
-		Surf_ptr yplane1 = make_shared<plane> ("yplane1", 0.0, 1.0, 0.0, -40.0);
-		Surf_ptr yplane2 = make_shared<plane> ("yplane2", 0.0, 1.0, 0.0, 40.0);
-		Surf_ptr zplane1 = make_shared<plane> ("zplane1", 0.0, 0.0, 1.0, -40.0);
-		Surf_ptr zplane2 = make_shared<plane> ("zplane2", 0.0, 0.0, 1.0, 40.0);
+		Surf_ptr sphere1 = make_shared<sphere>("sphere1", 0.0, 0.0, 0.0, 1.4935*0.0254);
+		Surf_ptr sphere2 = make_shared<sphere>("sphere2", 0.0, 0.0, 0.0, 4.4935*0.0254);
+		Surf_ptr xplane1 = make_shared<plane> ("xplane1", 1.0, 0.0, 0.0, -40.0*0.0254);
+		Surf_ptr xplane2 = make_shared<plane> ("xplane2", 1.0, 0.0, 0.0, 40.0*0.0254);
+		Surf_ptr yplane1 = make_shared<plane> ("yplane1", 0.0, 1.0, 0.0, -40.0*0.0254);
+		Surf_ptr yplane2 = make_shared<plane> ("yplane2", 0.0, 1.0, 0.0, 40.0*0.0254);
+		Surf_ptr zplane1 = make_shared<plane> ("zplane1", 0.0, 0.0, 1.0, -40.0*0.0254);
+		Surf_ptr zplane2 = make_shared<plane> ("zplane2", 0.0, 0.0, 1.0, 40.0*0.0254);
 
 		surfaces.push_back(sphere1);
 		surfaces.push_back(sphere2);
@@ -148,7 +148,7 @@ void Geometry::setup( std::string filename , int num_groups, bool loud )
 		vector<double> sourceGroups;
 		sourceGroups.push_back(1.0);
 		sourceGroups.push_back(0.0);
-		source = make_shared<setSourceSphere>(0.0, 0.0, 0.0, 0.0, 1.4935, sourceGroups);
+		source = make_shared<setSourceSphere>(0.0, 0.0, 0.0, 0.0, 1.4935*0.0254, sourceGroups);
 
 
 	}
