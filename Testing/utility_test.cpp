b77cd7a4c6d3eb1601766ec97d71ee20656b7bdd
esgonzalez <evangonzalez.mail@gmail.com>
2018-03-22 13:54:42 -0400

    Added a bit to utility_test.cpp

--- Testing/utility_test.cpp
+++ Testing/utility_test.cpp
@@ -10,23 +10,31 @@
 TEST_CASE( "Utility", "[utility]" ) {
 
     std::vector< int    > intVec;
-    std::vector< double > doubleVec;
+    std::vector< double > doubleVec, doubleVec2;
 
     for (int i=1; i<5; i++)
     {
       intVec.push_back( i );
       doubleVec.push_back( i * 1.0 );
+      doubleVec2.push_back( i * 1.0 + 4.0 );
     }
 
     
-    // test returns sum of integer vector
-    SECTION ( " sum a vector of integers " ) {
+    // test returns sum of vector
+    SECTION ( " sum a vector " ) {
       REQUIRE( Utility::vecSum( intVec ) == 10 );
+      REQUIRE( Utility::vecSum( doubleVec ) == 10.0 );
     }
 
-    // test returns sum of double vector
-    SECTION ( " sum a vector of doubles " ) {
-      REQUIRE( Utility::vecSum( doubleVec ) == 10.0 );
+    // test returns mean of vector
+    SECTION ( " return mean of a vector " ) {
+      // failed for intVec
+      REQUIRE( Utility::vecMean( doubleVec ) == 2.5 );
+    }
+
+    // test returns dot product of vector
+    SECTION ( " return mean of a vector " ) {
+      REQUIRE( Utility::vecDot( doubleVec, doubleVec2 ) == 70 );
     }
 
 }
